64c392e25c29ca0b22ba2723caadb1522f290fc9
dewabrata <dewabrata@gmail.com>
2022-04-26 11:50:23 +0700

    add Registration function

--- app/src/main/java/com/adl/authenticationfirebase/Register.kt
+++ app/src/main/java/com/adl/authenticationfirebase/Register.kt
@@ -2,10 +2,27 @@ package com.adl.authenticationfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_register.*
 
 class Register : AppCompatActivity() {
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        auth = FirebaseAuth.getInstance()
+        btnRegUser.setOnClickListener({
+            register()
+        })
+    }
+
+    private fun register() {
+      auth.createUserWithEmailAndPassword(txtEmail.text.toString(),txtRegPassword.text.toString()).addOnCompleteListener{
+          it ->
+          if(it.isSuccessful){
+              finish()
+          }
+      }.addOnFailureListener { exception -> Toast.makeText(applicationContext,exception.localizedMessage,Toast.LENGTH_LONG).show() }
     }
 }
